14bc0e5a946d08ba6ef46a2c5c6f2bb42f10393f
Solomiya Pylypiv <sololita@umich.edu>
2025-04-08 08:44:00 -0400

    Initial Commit

---
+++ CornCarRental/CornCarRentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornCarRentalApp.swift
+//  CornCarRental
+//
+//  Created by Solomiya Pylypiv on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CornCarRentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CornCarRentalTests/CornCarRentalTests.swift
@@ -0,0 +1,17 @@
+//
+//  CornCarRentalTests.swift
+//  CornCarRentalTests
+//
+//  Created by Solomiya Pylypiv on 4/8/25.
+//
+
+import Testing
+@testable import CornCarRental
+
+struct CornCarRentalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
